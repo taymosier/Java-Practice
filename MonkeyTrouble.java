9c3545607c5411bdffc2a075f45bc2e630f1aa59
Taylor Mosier <taymosier@gmail.com>
2017-04-18 20:08:59 -0400

    Add files via upload

---
+++ MonkeyTrouble.java
@@ -0,0 +1,19 @@
+/*
+ * 
+We have two monkeys, a and b, and the parameters 
+aSmile and bSmile indicate if each is smiling. 
+We are in trouble if they are both smiling or if 
+neither of them is smiling. Return true if we 
+are in trouble.
+ */
+
+
+public class MonkeyTrouble {
+	
+	public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+		  if (aSmile != bSmile){
+		    return false;
+		  }
+		  return true;
+		}
+}
